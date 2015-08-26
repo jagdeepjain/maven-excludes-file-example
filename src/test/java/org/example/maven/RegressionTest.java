4527c7277d4215296d390b1f7c3a17228e677ffb
Jagdeep Jain <jagdeep.jain@gmail.com>
2015-08-26 20:53:23 +0530

    first commit

---
+++ src/test/java/org/example/maven/RegressionTest.java
@@ -0,0 +1,38 @@
+package org.example.maven;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class RegressionTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public RegressionTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( RegressionTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
